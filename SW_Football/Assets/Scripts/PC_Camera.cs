f856761a6e355f39677941355cd68733b36981a1
Timothy Coish <tdcoish@gmail.com>
2019-06-26 22:43:35 -0700

    First Commit
    I think a lot of junk got added. As for features, just spent 15 minutes on the basics of a camera.

---
+++ SW_Football/Assets/Scripts/PC_Camera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC_Camera : MonoBehaviour
+{
+    private Camera                  mCam;
+
+    private void Start()
+    {
+        mCam = GetComponent<Camera>();
+        if(!mCam){
+            Debug.Log("No Cam Found");
+        }
+    }
+
+    // attach a camera to whatever object you put this in.
+    private void Update(){
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        // rotate camera view around y axis.
+        transform.RotateAround(transform.position, Vector3.up, mouseX);
+        Vector3 xAx = Vector3.Cross(transform.forward, Vector3.up);
+        transform.RotateAround(transform.position, xAx, mouseY);
+    }
+    
+    private void Orient(){
+
+    }
+
+
+}
