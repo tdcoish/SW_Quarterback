68f4b578cfa3bcc13215c67c0ff3a93e86ec46c1
Timothy Coish <tdcoish@gmail.com>
2019-06-27 22:11:18 -0700

    THROWING
    Target Dummy added.

---
+++ SW_Football/Assets/Scripts/AI_MovingTarget.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class AI_MovingTarget : MonoBehaviour
+{
+    private Rigidbody           mRigid;
+
+    private float               mTime = 5f;
+    private float               mLastChange = 0f;
+    private float               mSpd = 2f;
+    private bool                mGoRight = true;
+
+    void Start()
+    {
+        mRigid = GetComponent<Rigidbody>();    
+    }
+
+    void Update()
+    {
+        
+        if(mGoRight){
+            mRigid.velocity = transform.right * mSpd;
+        }else{
+            mRigid.velocity = transform.right * -mSpd;
+        }
+
+        if(Time.time - mLastChange > mTime){
+            mGoRight = !mGoRight;
+            mLastChange = Time.time;
+        }
+    }
+}
--- SW_Football/Assets/Scripts/PROJ_Football.cs
+++ SW_Football/Assets/Scripts/PROJ_Football.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PROJ_Football : MonoBehaviour
 {
+    private Rigidbody               mRigid;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mRigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 vel = mRigid.velocity;
+        transform.rotation = Quaternion.LookRotation(vel);
     }
 }
