b4013edf4ae7bbe4663f2e01879715affe480b16
Timothy Coish <tdcoish@gmail.com>
2019-07-02 10:44:13 -0700

    PLAYS
    Reciever can have a path built, which they will move along. Currently very clunky.

---
+++ SW_Football/Assets/Scripts/AI/AI_Route.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Route : MonoBehaviour
+{
+
+    public List<AI_RouteNode>           mPath;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ SW_Football/Assets/Scripts/AI/AI_RouteFollow.cs
@@ -0,0 +1,40 @@
+﻿/*********************************************************
+AI has a path, it follows the nodes of the path, until it hits one,
+then it goes to the next node.
+****************************************************** */
+
+using UnityEngine;
+using System.Collections.Generic;
+
+public class AI_RouteFollow : MonoBehaviour
+{
+    private Rigidbody           mRigid;
+
+    private float               mSpd = 2f;
+
+    // for now setting in the editor, eventually it will be a hash or string that is used to load in the route.
+    public List<AI_RouteNode>       mPath;
+
+    void Start()
+    {
+        mRigid = GetComponent<Rigidbody>();    
+    }
+
+    void Update()
+    {
+        if(mPath.Count == 0){
+            mRigid.velocity = Vector3.zero;
+            return;
+        }
+
+        // set it's rotation to the next node.
+        transform.rotation = Quaternion.LookRotation((mPath[0].transform.position - transform.position), Vector3.up);
+
+        mRigid.velocity = transform.forward * mSpd;
+
+        // if we're somewhat close to our node, then now move to the next one.
+        if(Vector3.Distance(transform.position, mPath[0].transform.position) < 3f){
+            mPath.RemoveAt(0);
+        }
+    }
+}
