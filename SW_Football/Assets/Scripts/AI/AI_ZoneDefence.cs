99f6bbf5c18aaaae0953d1457a52232e300fe114
Timothy Coish <tdcoish@gmail.com>
2019-07-10 21:44:41 -0700

    AI
    Added different way for AI to break on the ball. Basically they are just going directly to the path of the ball, ignoring the height of the ball. This leads to very good behaviour on short passes, not so great on deep bombs.

--- SW_Football/Assets/Scripts/AI/AI_ZoneDefence.cs
+++ SW_Football/Assets/Scripts/AI/AI_ZoneDefence.cs
@@ -27,14 +27,8 @@ public class AI_ZoneDefence : MonoBehaviour
     {
         if(!mBreakOnBall) return;
 
-        Debug.Log("setting vel");
-
-        // calculate the straight distance.
-        Vector3 dis = rValidFootball.Val - transform.position;
-        Debug.Log("Distance: " + dis);
-        dis = Vector3.Normalize(dis);
-        cRigid.velocity = dis * mMaxVel;
-        Debug.Log("Vel: " + cRigid.velocity);
+        // GoDirectlyToBall();
+        InterceptBall();
     }
 
     // triggered by ball thrown event
@@ -42,5 +36,32 @@ public class AI_ZoneDefence : MonoBehaviour
     {
         Debug.Log("Breaking on ball");
         mBreakOnBall = true;
+    }
+
+    // stupidest one, they just run straight to the ball. Leads to them always moving forwards
+    void GoDirectlyToBall()
+    {
+        // calculate the straight distance.
+        Vector3 dis = rValidFootball.Val - transform.position;
+        dis = Vector3.Normalize(dis);
+        cRigid.velocity = dis * mMaxVel;
+    }
+
+    // Ignores other players. They find the shortest path based on the football velocity and move there.
+    void InterceptBall()
+    {
+        // This is slow, but it's decent for debugging.
+        // for now we can just hack it with the football.transform.right vector, but it will get more complicated later.
+        PROJ_Football fBallRef = FindObjectOfType<PROJ_Football>();
+
+        Vector3 dis = fBallRef.transform.position - transform.position;
+        dis = Vector3.Normalize(dis);
+
+        // go to the right.
+        if(Vector3.Dot(fBallRef.transform.right, dis) >= 0f){
+            cRigid.velocity = mMaxVel * fBallRef.transform.right;
+        }else{
+            cRigid.velocity = mMaxVel * -fBallRef.transform.right;
+        }
     }
 }
