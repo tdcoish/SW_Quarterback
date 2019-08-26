368fd73e217325460f55ea8a4e8125cae022e798
Timothy Coish <tdcoish@gmail.com>
2019-08-26 12:36:12 -0700

    EDITOR_Movement
    Can snap the players position around using <^>v arrow buttons.

---
+++ SW_Football/Assets/Scripts/Play Editor/PE_Repositioner.cs
@@ -0,0 +1,70 @@
+﻿/*************************************************************************************
+When they have selected a player, we reposition them using this script.
+*************************************************************************************/
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PE_Repositioner : MonoBehaviour
+{
+    private PE_Selector         cSelector;
+
+    [SerializeField]
+    private Text                rPositionTXT;
+
+    private void Start()
+    {
+        cSelector = GetComponent<PE_Selector>();
+    }
+    
+    public void BT_Up()
+    {
+        Debug.Log("Up pressed");
+        RepositionStart(new Vector2(0f, 1f));
+    }
+
+    public void BT_Down()
+    {
+        Debug.Log("Down pressed");
+        RepositionStart(new Vector2(0f, -1f));
+    }
+
+    public void BT_Right()
+    {
+        Debug.Log("Right pressed");
+        RepositionStart(new Vector2(1f, 0f));
+    }
+
+    public void BT_Left()
+    {
+        Debug.Log("Left pressed");
+        RepositionStart(new Vector2(-1f, 0f));
+    }
+
+    private void RepositionStart(Vector2 vMovement)
+    {
+        if(cSelector.mActivePlayer == -1)
+        {
+            Debug.Log("There is no active player");
+            return;
+        }
+
+        Debug.Log("Shift active player by: " + vMovement);
+        cSelector.rGuys[cSelector.mActivePlayer].GetComponent<PE_Role>().mStartPos += vMovement;
+        cSelector.rGuys[cSelector.mActivePlayer].transform.position += (Vector3)vMovement / 10f;        // hacked conversion.
+        // Again, unfortunately we have to convert from internal position to field position.
+
+        UpdatePositionText();
+    }
+
+    // Probably make this public.
+    private void UpdatePositionText()
+    {
+        if(cSelector.mActivePlayer == -1)
+        {
+            Debug.Log("There is no active player");
+            return;
+        }
+
+        rPositionTXT.text = "POS: " + cSelector.rGuys[cSelector.mActivePlayer].GetComponent<PE_Role>().mStartPos;
+    }
+}
---
+++ SW_Football/Assets/Scripts/TEST_Build.cs
@@ -0,0 +1,14 @@
+﻿/*************************************************************************************
+Outputs the application.dataPath
+*************************************************************************************/
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TEST_Build : MonoBehaviour
+{
+    public Text             mTXT;
+    void Start()
+    {
+        mTXT.text = "PATH: " + Application.dataPath;
+    }
+}
