f4029b0f0e37613c53fa462e91d643e6d2df897a
Timothy Coish <tdcoish@gmail.com>
2019-09-04 18:56:32 -0700

    Momentum/Collisions
    Staging before big change

--- SW_Football/Assets/Scripts/Practice/PRAC_Ath.cs
+++ SW_Football/Assets/Scripts/Practice/PRAC_Ath.cs
@@ -55,4 +55,54 @@ public class PRAC_Ath : MonoBehaviour
     {
 
     }
+
+    /******************************************************************
+    You hit me with your weight*differenceInVelocity. If I am going 5m/s north,
+    and you are going 4m/s north, you hit me at 1m/s * your weight.
+
+    Component of their velocity into us, minus the component of our velocity, in that 
+    direction.
+
+    Due to ordering effects, we should in fact change their velocity for them. Yes, this does
+    have some bugs (they get effected twice per frame), but it makes the collisions
+    "fair", as in, it doesn't matter who gets collided with first.
+    ******************************************************************/
+    void OnCollisionStay(Collision collision)
+    {
+        if(collision.transform.GetComponent<PRAC_Ath>() != null)
+        {
+            // The direction from them to us.
+            Vector3 vDirThemToUs = transform.position - collision.transform.position;
+            vDirThemToUs = Vector3.Normalize(vDirThemToUs);
+
+            // Now get the momentum they have.
+            float mWgt = 100f;          // just giving everyone 100 lbs for now.
+
+            Vector3 vTheirVelIntoUs = vDirThemToUs * Vector3.Dot(collision.rigidbody.velocity, vDirThemToUs);
+            Vector3 vOurVelAwayFromThem = vDirThemToUs * Vector3.Dot(cRigid.velocity, vDirThemToUs);
+
+            Vector3 vTheirRelativeVelIntoUs = vTheirVelIntoUs - vOurVelAwayFromThem;
+
+            if(Vector3.Dot(vTheirRelativeVelIntoUs, vDirThemToUs) < 0f)
+            {
+                Debug.Log("Probably due to ordering, they are not moving into us");
+                return;
+            }
+
+            // although we do have to give a little more or we might have elastic collisions.
+            Vector3 vTheirForceIntoUs = vTheirRelativeVelIntoUs * mWgt;
+            
+            vTheirForceIntoUs /= mWgt;                  // simulating our weight dampening a blow.
+
+            vTheirForceIntoUs *= 100f;           // debugging.
+
+            cRigid.velocity += vTheirForceIntoUs;
+        }
+    }
+
+    // We call this on the opposing athlete, as well.
+    public void FChangeVelAfterCol(PRAC_Ath other)
+    {
+        
+    }
 }
