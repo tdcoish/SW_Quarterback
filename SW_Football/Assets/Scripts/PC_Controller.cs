67659e252ff0be3ae7606abf275f60021c9b540a
Timothy Coish <tdcoish@gmail.com>
2019-09-06 12:55:14 -0700

    Throwing
    Everything but the pre-throw limiter added.

--- SW_Football/Assets/Scripts/PC_Controller.cs
+++ SW_Football/Assets/Scripts/PC_Controller.cs
@@ -193,8 +193,9 @@ public class PC_Controller : MonoBehaviour
 
         // Alright, this is what needs to be changed. Throw power should not charge linearly, it should charge logarithmically.
         float fChrgPct = mThrowChrg.Val;
-        float fChargeAmt = Time.deltaTime * (1/IO_Settings.mSet.lPlayerData.mThrowChargeTime);
-        fChargeAmt -= fChargeAmt*(fChrgPct);           // gives us right side of bell curve.
+        float fChargeAmt = Time.deltaTime / IO_Settings.mSet.lPlayerData.mReleaseTime;
+        // float fChargeAmt = Time.deltaTime / 5f;
+        fChargeAmt -= fChargeAmt*(fChrgPct*fChrgPct);           // gives us right side of bell curve.
         // but now we also have to factor in that we charge faster when closer to 0.
         // fChargeAmt *= (1-mThrowChrg.Val) * 2f;              // so when at 0, x as fast. When at 1, 0 charge speed.
         mThrowChrg.Val += fChargeAmt;
