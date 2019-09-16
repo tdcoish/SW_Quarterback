4c39da8845bf342c28c21c6c20627bf4b052e6e3
Timothy Coish <tdcoish@gmail.com>
2019-09-16 14:07:04 -0700

    Route Editor
    Placing things on grid is working.

---
+++ SW_Football/Assets/Scripts/RouteEditor/ED_RT_Grid.cs
@@ -0,0 +1,36 @@
+﻿/*************************************************************************************
+Should be able to access the grid through x,y coordinates.
+*************************************************************************************/
+using UnityEngine;
+
+public class ED_RT_Grid : MonoBehaviour
+{
+
+    private ED_RT_Square[]              mSquares;
+    public int                          mSqrLnth = 15;
+    public int                          squarePixelSize = 20;
+
+    void Start()
+    {
+        mSquares = GetComponentsInChildren<ED_RT_Square>();
+
+        for(int i=0; i<mSquares.Length; i++)
+        {
+            Vector2 v = mSquares[i].transform.position;
+            v.x += (i%mSqrLnth)*squarePixelSize;
+            v.y -= (i/mSqrLnth)*squarePixelSize;
+            mSquares[i].transform.position = v;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public Vector3 FGetPos(int x, int y)
+    {
+        int el = y*mSqrLnth + x;
+        return mSquares[el].transform.position;
+    }
+}
---
+++ SW_Football/Assets/Scripts/RouteEditor/ED_RT_Man.cs
@@ -0,0 +1,61 @@
+﻿/*************************************************************************************
+
+*************************************************************************************/
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ED_RT_Man : MonoBehaviour
+{
+    public enum STATE{
+        S_BEGIN,
+        S_PLACING,
+        S_END
+    }
+    public STATE                                    mState;
+
+    public Image                                    PF_Player;
+    
+    public ED_RT_Grid                               rGrid;
+
+    void Update()
+    {
+
+        switch(mState){
+            case STATE.S_BEGIN: RUN_BEGIN(); break;
+            case STATE.S_PLACING: RUN_PLACING(); break;
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Input.mousePosition, Vector2.zero);
+            
+            if(hit.collider != null)
+            {
+                if(hit.collider.GetComponent<ED_RT_Square>() != null)
+                {
+                    Debug.Log("Hit grid square");
+                    Debug.Log(hit.transform.position);
+                    var clone = Instantiate(PF_Player, hit.transform.position, transform.rotation);
+                    clone.rectTransform.SetParent(rGrid.transform);
+                }
+            }
+
+        }
+    }
+    
+    // I first place a receiver in a certain spot, and that's it.
+    private void RUN_BEGIN()
+    {
+        Vector3 vRecPos = rGrid.FGetPos(rGrid.mSqrLnth/2 + 1, rGrid.mSqrLnth-1);
+        var clone = Instantiate(PF_Player, vRecPos, transform.rotation);
+        clone.rectTransform.SetParent(rGrid.transform);
+
+        mState = STATE.S_PLACING;
+    }
+
+    private void RUN_PLACING()
+    {
+
+    }
+
+}
