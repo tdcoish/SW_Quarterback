f9d6b6ee809358d8a86c7f248d6156fef6b4d267
Timothy Coish <tdcoish@gmail.com>
2019-09-27 14:16:28 -0700

    Animations
    AI animations, kind of getting the hang of things.

---
+++ SW_Football/Assets/Scripts/Prac_Offense/AN_OffPlayer.cs
@@ -0,0 +1,45 @@
+﻿/*************************************************************************************
+
+*************************************************************************************/
+using UnityEngine;
+
+public class AN_OffPlayer : MonoBehaviour
+{
+
+    private PRAC_Off_Ply                    cPlayer;
+    private Animator                        cAnimator;
+
+    void Start()
+    {
+        cPlayer = GetComponent<PRAC_Off_Ply>(); 
+        cAnimator = GetComponentInChildren<Animator>();   
+    }
+
+    // Switch based on the state the player is in?
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P)){
+            SetAllBoolsToFalse();
+            cAnimator.SetBool("isStrafing", true);
+        }
+        if(Input.GetKeyDown(KeyCode.O)){
+            SetAllBoolsToFalse();
+            cAnimator.SetBool("isPreSnap", true);
+        }
+        if(Input.GetKeyDown(KeyCode.U)){
+            SetAllBoolsToFalse();
+            cAnimator.SetBool("isRunning", true);
+        }
+
+    }
+
+    void SetAllBoolsToFalse()
+    {
+        Debug.Log("Setting all bools to false");
+        cAnimator.SetBool("isActive", false);
+        cAnimator.SetBool("isRunning", false);
+        cAnimator.SetBool("isPreSnap", false);
+        cAnimator.SetBool("isStrafing", false);
+        cAnimator.SetBool("isDoneRoute", false);
+    }
+}
