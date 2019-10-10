88625796ab87fef288a5a0c9cec5905f66f23ad4
Timothy Coish <tdcoish@gmail.com>
2019-10-10 14:00:27 -0700

    PRAC/AUD
    Reworked the audio two have two different sources. One for the announcer, one for the effects.

--- SW_Football/Assets/Scripts/Prac_Offense/AD_Prac.cs
+++ SW_Football/Assets/Scripts/Prac_Offense/AD_Prac.cs
@@ -1,40 +1,53 @@
 ﻿/*************************************************************************************
-
+Glad I'm doing this now. There are clear scaling issues going forwards.
+These lists should be made in text files, not here, or in the editor.
 *************************************************************************************/
 using UnityEngine;
 using System.Collections.Generic;
 
 public class AD_Prac : MonoBehaviour
 {
-    public AudioSource                          mAudioSrc;
+    public AudioSource                          mAnnouncer;
+    public AudioSource                          mFX;    
 
-    public List<ClipAndGivenName>               mClips = new List<ClipAndGivenName>();
+    public List<ClipAndGivenName>               mAnnClips = new List<ClipAndGivenName>();
+    public List<ClipAndGivenName>               mFXClips = new List<ClipAndGivenName>();
 
-    public void FPlayClip(string name)
+    public void FPlayClipAnnouncer(string name)
     {
-        for(int i=0; i<mClips.Count; i++){
-            if(mClips[i].mName == name){
-                mAudioSrc.clip = mClips[i].mClip;
-                mAudioSrc.Play();
+        for(int i=0; i<mAnnClips.Count; i++){
+            if(mAnnClips[i].mName == name){
+                mAnnouncer.clip = mAnnClips[i].mClip;
+                mAnnouncer.Play();
+            }
+        }
+    }
+    
+    public void FPlayClipFX(string name)
+    {
+        for(int i=0; i<mFXClips.Count; i++){
+            if(mFXClips[i].mName == name){
+                mFX.clip = mFXClips[i].mClip;
+                mFX.Play();
             }
         }
     }
 
     public void FPlayWhistle()
     {
-        FPlayClip("Whistle");
+        FPlayClipFX("Whistle");
     }
 
     public void FPlayOver(bool bRecCaught, bool bInt)
     {
         Debug.Log("Playing over");
         if(bInt){
-            FPlayClip("PD0");
+            FPlayClipAnnouncer("PD0");
         }else{
             if(bRecCaught){
-                FPlayClip("PLY_Good0");
+                FPlayClipAnnouncer("PLY_Good0");
             }else{
-                FPlayClip("PLY_Fail0");
+                FPlayClipAnnouncer("PLY_Fail0");
             }
         }
     }
